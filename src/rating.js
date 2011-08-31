eeef8f9e7ad53e0bb316e8ccb901a8cc53d78666
Ray Nicholus <ray+git@widen.com>
2011-08-30 20:41:00 -0500

    get domain of current tab, todo: get company name via allwhois.com & use for BBB search

--- src/rating.js
+++ src/rating.js
@@ -1,31 +1,70 @@
-        var currentTab  =  {
-            id:undefined,
-            url:undefined
-        }
-
-        chrome.tabs.onSelectionChanged.addListener(function(tabId, selectInfo) {
-            console.log('old tabId: ' + currentTab.id);
-            currentTab.id = tabId;
-            console.log('new tab id: ' + currentTab.id);
-        });
-
-        chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-            if (tabId == currentTab.id && changeInfo.status == 'loading') {
-                currentTab.url = changeInfo.url;
-                console.log('new url: ' + currentTab.url);
-            }
-        });
+var domainPattern = /http[s]?:\/\/(\w+\.)*(\w+\.\w+)/\/;
+var lastDomainStr;
 
 
-        <!--$('#result').load('http://wisconsin.bbb.org/Find-Business-Reviews/url/koss_com/ tr.result-row', function(data) {-->
-            <!--var link = $('#result').find('.biz-accred-status').find('a').attr('href') + ' #accedited-rating';-->
-            <!--$('#result').load(link, function() {-->
-                <!--var title = $('#result').find('img').attr('title');-->
-                <!--var myRegexp = /BBB.*\s(.+)\sRating/;-->
-                <!--var match = myRegexp.exec(title);-->
-                <!--var rating = match[1];-->
-                <!--chrome.browserAction.setBadgeText({-->
-                    <!--text:rating-->
-                <!--});-->
-            <!--});-->
-        <!--});-->
+var currentTab  =  {
+	id:undefined,
+    url:undefined
+}
+
+chrome.tabs.onSelectionChanged.addListener(function(tabId, selectInfo) {
+	if (tabId != currentTab.id)
+	{
+		currentTab.id = tabId;
+		chrome.tabs.get(currentTab.id, function (tab) {
+			var domainStr = getDomain(tab.url);
+			if (domainStr && domainStr != lastDomainStr)
+			{
+				console.log("new tab w/ new domain: " + domainStr);
+				console.log(getDomainAsSearchContext(domainStr));
+				lastDomainStr = domainStr;
+			}
+		});
+	}
+});
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+	if (tabId == currentTab.id && changeInfo.status == 'loading') {
+		currentTab.url = changeInfo.url;
+		var domainStr = getDomain(currentTab.url);
+		if (domainStr && domainStr != lastDomainStr)
+		{
+			console.log('new domain: ' + domainStr);
+			console.log(getDomainAsSearchContext(domainStr));
+			lastDomainStr = domainStr;
+		}
+	}
+});
+		
+function getDomain(urlStr)
+{
+	var match = domainPattern.exec(urlStr);
+	if (match)
+	{
+		return match[2];
+	}
+}
+
+function getDomainAsSearchContext(domainStr)
+{
+	return domainStr.replace('\.', '_');
+}
+
+function getCompanyName(domainStr)
+{
+	//TODO, using allwhois.com
+}
+
+
+<!--$('#result').load('http://wisconsin.bbb.org/Find-Business-Reviews/url/koss_com/ tr.result-row', function(data) {-->
+	<!--var link = $('#result').find('.biz-accred-status').find('a').attr('href') + ' #accedited-rating';-->
+	<!--$('#result').load(link, function() {-->
+		<!--var title = $('#result').find('img').attr('title');-->
+		<!--var myRegexp = /BBB.*\s(.+)\sRating/;-->
+		<!--var match = myRegexp.exec(title);-->
+		<!--var rating = match[1];-->
+		<!--chrome.browserAction.setBadgeText({-->
+			<!--text:rating-->
+		<!--});-->
+	<!--});-->
+<!--});-->
